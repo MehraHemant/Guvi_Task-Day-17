d67aae03a6fbcbc107edfbb8e71ed7a8a37d724d
MehraHemant <hemantmehra9502@gmail.com>
2023-11-04 12:22:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+let timer = 10;
+      let time = document.getElementById("display");
+      time.style.margin = "auto";
+      time.style.textAlign = "center";
+      time.style.maxWidth = "200px";
+      time.style.height = "200px";
+      setTimeout(() => {
+        time.innerHTML = timer--;
+        setTimeout(() => {
+          time.innerHTML = timer--;
+          setTimeout(() => {
+            time.innerHTML = timer--;
+            setTimeout(() => {
+              time.innerHTML = timer--;
+              setTimeout(() => {
+                time.innerHTML = timer--;
+                setTimeout(() => {
+                  time.innerHTML = timer--;
+                  setTimeout(() => {
+                    time.innerHTML = timer--;
+                    setTimeout(() => {
+                      time.innerHTML = timer--;
+                      setTimeout(() => {
+                        time.innerHTML = timer--;
+                        setTimeout(() => {
+                          time.innerHTML = timer--;
+                          setTimeout(() => {
+                            time.style.fontSize = "36px";
+                            time.style.maxWidth = "310px";
+
+                            time.innerHTML = "Happy Independence Day!";
+                          }, 1000);
+                        }, 1000);
+                      }, 1000);
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
